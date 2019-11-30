3ada7f672f9644d87389621d282b2de3e97b0548
Fvsen <532482829@qq.com>
2019-11-30 20:48:21 +0800

    Initial commit

---
+++ wx_server/src/test/java/com/fvsen/wx_server/WxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fvsen.wx_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
